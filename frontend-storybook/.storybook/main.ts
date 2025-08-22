444f45db634b9a729da364fd084cdfec213aac69
mhanans <marius.hanan@hq.accelist.com>
2025-08-22 10:29:20 +0700

    implement storybook

--- frontend-storybook/.storybook/main.ts
+++ frontend-storybook/.storybook/main.ts
@@ -1,18 +1,23 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials'
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs-vite",
+    "options": {}
   },
-  docs: {
-    autodocs: 'tag'
-  }
+  "staticDirs": [
+    "..\\public"
+  ]
 };
-
 export default config;
